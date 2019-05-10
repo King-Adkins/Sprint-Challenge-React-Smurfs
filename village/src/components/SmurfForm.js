993bc5f193d2611c1cfc34963ec3ae7cb085f37d
King Adkins <lambda_school_loaner_87@Lambda-School-Loaner-87s-MacBook-Pro.local>
2019-05-10 11:35:29 -0400

    Add some style using less files to complie css

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -36,25 +36,25 @@ class SmurfForm extends Component {
     return (
       <div className="SmurfForm">
         <form onSubmit={this.addSmurf}>
-          <input
+          <input className = 'SmurfForm-input'
             onChange={this.handleInputChange}
             placeholder="name"
             value={this.state.name}
             name="name"
           />
-          <input
+          <input className = 'SmurfForm-input'
             onChange={this.handleInputChange}
             placeholder="age"
             value={this.state.age}
             name="age"
           />
-          <input
+          <input className = 'SmurfForm-input'
             onChange={this.handleInputChange}
             placeholder="height"
             value={this.state.height}
             name="height"
           />
-          <button type="submit">Add to the village</button>
+          <button className = 'SmurfFrom-button' type="submit">Add to the village</button>
         </form>
       </div>
     );
